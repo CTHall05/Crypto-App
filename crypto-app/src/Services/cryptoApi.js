0f01b943ae370500cf28e8fd277d5cc6dea10b2f
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-03-13 05:08:55 +0000

    Redux Toolkit & API dev added to the project, getting basic crypto data for the homepage

---
+++ crypto-app/src/Services/cryptoApi.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import thunk from 'redux-thunk';
+
+export const cryptoApiHeaders = {
+  'X-RapidAPI-Key': 'e7b7662f20msh50f2a2ac750efa9p1d8753jsn933b860e16b4',
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+const middleware = [thunk];
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl}),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest(`/coins`)
+    })
+  }),
+  middleware
+});
+
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
